3cab2fbbdd90edc143177ecce98b24616f7cb92a
Admin <ge1983@yandex.ru>
2023-03-05 17:16:25 +1000

    Добавлено задача 3 для ДЗ

---
+++ DZ3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Ввелите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=N; i++)
+{
+    Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+}
